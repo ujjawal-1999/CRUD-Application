66f3dc498ab4d8bca736f44d68bc1063a6d39a7e
Ujjawal <jainujjawal1999@gmail.com>
2021-10-04 22:50:40 +0530

    Enhanced Editor features

--- config/passport.js
+++ config/passport.js
@@ -2,41 +2,45 @@ const GoogleStrategy = require("passport-google-oauth20");
 const mongoose = require("mongoose");
 const User = require("../models/User");
 
-module.exports = function(passport){
-    passport.use(new GoogleStrategy({
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://afternoon-caverns-94241.herokuapp.com/auth/google/callback'
-    },
-    async (accessToken,refreshToken,profile,done)=>{
+        callbackURL:
+          "https://afternoon-caverns-94241.herokuapp.com/auth/google/callback",
+        // callbackURL: "http://localhost:3000/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
         const newUser = {
-            googleId: profile.id,
-            displayName:profile.displayName,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            image: profile.photos[0].value
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.log("Error: ", err);
         }
-        try{
-            let user = await User.findOne({googleId: profile.id});
-            if(user){
-                done(null,user);
-            }else{
-                user = await User.create(newUser);
-                done(null,user);
-            }
-        }   
-        catch(err){
-            console.log('Error: ',err);
-        }
+      }
+    )
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-    }))
-    passport.serializeUser((user, done)=> {
-        done(null, user.id);
-      });
-      
-      passport.deserializeUser((id, done)=> {
-        User.findById(id, (err, user)=> {
-          done(err, user);
-        });
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
     });
-}
+  });
+};
